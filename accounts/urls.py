ba1bce524b32c6d9ef6fc98fdfac4153a8376617
jejoonlee <jejoonlee1996@gmail.com>
2022-10-21 13:35:59 +0900

    account_update.html / password_update_html / login.html

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = "accounts"
 
-urlpattenrs = [
+urlpatterns = [
     path("signup/", views.signup, name="signup"),
     path("login/", views.login, name="login"),
     path("logout/", views.logout, name="logout"),
