2b8d2c01b05c6e89901a987c02724e14a99f9c06
midhyun <kfree912@gmail.com>
2022-10-21 11:31:43 +0900

    articles/fix_am

--- articles/views.py
+++ articles/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect
-from .models import Articles, Comments
+from .models import Review, Comment
 from .forms import ArticleForm, CommentForm
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
-    articles = Articles.objects.all()
+    articles = Review.objects.all()
     context = {
         'articles': articles
     }
@@ -16,7 +16,9 @@ def create(request):
     if request.method == 'POST':
         form = ArticleForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            temp = form.save(commit=False)
+            temp.user = request.user
+            temp.save()
             return redirect('articles:index')
     else:
         form = ArticleForm()
@@ -27,7 +29,7 @@ def create(request):
 
 @login_required
 def update(request, pk):
-    article = Article.objects.get(pk=pk)
+    article = Review.objects.get(pk=pk)
     if request.user == article.user:
         if request.method == 'POST':
             form = ArticleForm(request.POST, request.FILES, instance=article)
@@ -46,13 +48,13 @@ def update(request, pk):
 @login_required
 def delete(request, pk):
     if request.user.pk == pk:
-        Article.objects.get(pk=pk).delete()
+        Review.objects.get(pk=pk).delete()
         return redirect('articles:index')
     else:
         return redirect('articles:error')
 
 def detail(request, pk):
-    article = Article.objects.get(pk=pk)
+    article = Review.objects.get(pk=pk)
     context = {
         'aritcle': article
     }
@@ -64,13 +66,14 @@ def comments(request, article_pk):
         form = CommentForm(request.POST)
         if form.is_valid():
             temp = form.save(commit=False)
+            temp.review_id = article_pk
             temp.user = request.user
             temp.save()
             return redirect('articles:detail', article_pk)
 
 @login_required
 def comments_delete(request, article_pk, pk):
-    comment = Comments.objects.get(pk=pk)
+    comment = Comment.objects.get(pk=pk)
     if comment.user == request.user:
         comment.delete()
         return redirect('articles:detail', article_pk)
