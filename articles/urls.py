e4aa545bcb940517ffa2ab0e99766384ca80bfe8
midhyun <kfree912@gmail.com>
2022-10-21 14:08:53 +0900

    article_bug

--- articles/urls.py
+++ articles/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path("<int:pk>/update", views.update, name="update"),
     path("<int:pk>/delete", views.delete, name="delete"),
     path("<int:pk>", views.detail, name="detail"),
-    path("comments", views.comments, name="comments"),
+    path("<int:pk>/comments", views.comments, name="comments"),
     path(
         "<int:article_pk>/comments_delete/<int:pk>",
         views.comments_delete,
